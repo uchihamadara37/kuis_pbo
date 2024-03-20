bc23018ae75f65e58e9b5d4bf174faea93113b69
uchihamadara37 <54350630+uchihamadara37@users.noreply.github.com>
2024-03-20 14:16:41 +0700

    Add files via upload
    Andre kommit kuis

---
+++ kuis_pbo/src/main/java/com/mycompany/kuis_pbo/Kuis_pbo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.kuis_pbo;
+
+/**
+ *
+ * @author andre
+ */
+public class Kuis_pbo {
+
+    public static void main(String[] args) {
+        mainFrame myApp = new mainFrame();
+    }
+}
